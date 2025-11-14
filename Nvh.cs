a106080dd36880da012d223fbe3f688870f8ebd2
jaseian <jase.ian@outlook.com>
2025-11-14 12:07:30 +0800

    添加项目文件。

---
+++ Nvh.cs
@@ -0,0 +1,65 @@
+﻿using NvhLibCSharp.Interop;
+using System.Runtime.InteropServices;
+
+namespace NvhLibCSharp
+{
+    public static class Nvh
+    {
+        public static void LoadLicense(string licensePath)
+        {
+            var errCode = NvhInterop.LoadLicense(licensePath);
+            Assert(errCode);
+        }
+
+        public static double[] OverallLevelSpectral(Signal signal, int spectrumLines, double increment, double referenceValue, Window windowType, Weight weightType, Scale scaleType, out double[] timeAxis)
+        {
+            IntPtr dataPtr = IntPtr.Zero;
+            int bins = 0;
+            int errCode = NvhInterop.OverallLevelSpectral(signal, spectrumLines, increment, referenceValue, (int)windowType, (int)weightType, (int)scaleType, ref dataPtr, ref bins);
+            Assert(errCode);
+
+            double[] data = new double[bins];
+            Marshal.Copy(dataPtr, data, 0, bins);
+            Marshal.FreeCoTaskMem(dataPtr);
+
+            timeAxis = new double[bins];
+            for (int i = 0; i < bins; i++)
+            {
+                timeAxis[i] = i * increment;
+            }
+
+            return data;
+        }
+
+        public static double[] OrderSection(Signal signal, Rpm rpm, int spectrumLines, double targetOrder, double orderBandwidth, double minRpm, double maxRpm, double rpmStep, double referenceValue, Format formatType, Window windowType, Weight weightType, Scale scaleType, RpmTrigger rpmTriggerType, out double[] rpmAxis)
+        {
+            IntPtr dataPtr = IntPtr.Zero;
+            IntPtr rpmAxisPtr = IntPtr.Zero;
+            int bins = 0;
+            int errCode = NvhInterop.OrderSection(signal, rpm, spectrumLines, targetOrder, orderBandwidth, minRpm, maxRpm, rpmStep, referenceValue, (int)formatType, (int)windowType, (int)weightType, (int)scaleType, (int)rpmTriggerType, ref dataPtr, ref rpmAxisPtr, ref bins);
+            Assert(errCode);
+
+            double[] data = new double[bins];
+            Marshal.Copy(dataPtr, data, 0, bins);
+            Marshal.FreeCoTaskMem(dataPtr);
+
+            rpmAxis = new double[bins];
+            Marshal.Copy(rpmAxisPtr, rpmAxis, 0, bins);
+            Marshal.FreeCoTaskMem(rpmAxisPtr);
+
+            return data;
+        }
+
+        private static string GetLastErrorMessage(int errorCode)
+        {
+            return NvhInterop.GetLastErrorMessage(errorCode);
+        }
+
+        private static void Assert(int ret)
+        {
+            if (ret >= 0) return;
+            
+            throw new InvalidOperationException(GetLastErrorMessage(ret));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using NvhLibCSharp.Interop;
+using NvhLibCSharp.Utils;
+
+namespace NvhLibCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nvh.LoadLicense("D:\\测试\\LIC-20251114-3685ebd9.lic");
+
+            var samples = LoadData.Double("D:\\source\\NvhLibCSharp\\SampleData\\sound_signal_0.txt");
+            var rpmValues = LoadData.Double("D:\\source\\NvhLibCSharp\\SampleData\\speed_0.txt");
+
+            var signal = new Signal(samples, 1.0 / 51200.0);
+            var rpm = new Rpm(rpmValues, 1.0 / 51200.0);
+
+            var oaData = Nvh.OverallLevelSpectral(signal, 4096, 0.15, 2e-5, Window.Hanning, Weight.A, Scale.Linear, out var oaTimeAxis);
+            for (int i = 0; i < oaTimeAxis.Length; i++)
+            {
+                Console.WriteLine($"{oaTimeAxis[i]:F6}\t{oaData[i]:F6}");
+            }
+
+            var otsData = Nvh.OrderSection(signal, rpm, 4096, 14.0, 0.5, 1000, 4000, 25, 2e-5, Format.Rms, Window.Hanning, Weight.A, Scale.Linear, RpmTrigger.Up, out var otsRpmAxis);
+            for (int i = 0; i < otsRpmAxis.Length; i++)
+            {
+                Console.WriteLine($"{otsRpmAxis[i]:F6}\t{otsData[i]:F6}");
+            }
+        }
+    }
+}
