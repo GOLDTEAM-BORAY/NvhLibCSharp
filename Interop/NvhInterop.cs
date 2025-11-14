da7dd56ce9d44121ec18db9fb6e5dd5ed493bfca
jaseian <jase.ian@outlook.com>
2025-11-14 12:07:30 +0800

    添加项目文件。

---
+++ Interop/NvhInterop.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace NvhLibCSharp.Interop
+{
+    public static partial class NvhInterop
+    {
+        [LibraryImport("BrcSignalKit.dll", EntryPoint = "LoadLicense", StringMarshalling = StringMarshalling.Custom, StringMarshallingCustomType = typeof(AnsiStringMarshaller))]
+        public static partial int LoadLicense(string licensePath);
+
+        [LibraryImport("BrcSignalKit.dll", EntryPoint = "GetLastErrorMessage", StringMarshalling = StringMarshalling.Custom, StringMarshallingCustomType = typeof(AnsiStringMarshaller))]
+        public static partial string GetLastErrorMessage(int errorCode);
+
+        [LibraryImport("BrcSignalKit.dll", EntryPoint = "OverallLevelSpectral")]
+        public static partial int OverallLevelSpectral(Signal signal, int spectrumLines, double increment, double referenceValue, int windowType, int weightType, int scaleType, ref IntPtr data, ref int bins);
+
+        [LibraryImport("BrcSignalKit.dll", EntryPoint = "OrderSection")]
+        public static partial int OrderSection(Signal signal, Rpm rpm, int spectrumLines, double targetOrder, double orderBandwidth, double minRpm, double maxRpm, double rpmStep, double referenceValue, int formatType, int windowType, int weightType, int scaleType, int rpmTriggerType, ref IntPtr data, ref IntPtr rpmAxis, ref int bins);
+    }
+}
